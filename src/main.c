ea233bef53b0b97cee28408cb3f88c00deebc778
Cortan122 <Cortan122@gmail.com>
2025-01-15 03:34:51 +0100

    notes about preferredSize feature

--- src/main.c
+++ src/main.c
@@ -16,6 +16,15 @@ int main(int argc, char** argv) {
   Editor theEditor = {0};
   Editor$open(&theEditor, argv[1]);
 
+  // TODO: actually think about this feature
+  // have like an Editor$preferredSize
+  // and limit it to a maximum of DEFAULT_SCREENWIDTH, DEFAULT_SCREENHEIGHT
+  // and some reasonable minimums as well
+  // having it perfect looks kinda wrong as well
+  /* int width = theEditor.canvas.texture.width;
+  int height = theEditor.canvas.texture.height + 40;
+  SetWindowSize(width, height); */
+
   int i = 0;
   while (!WindowShouldClose()) {
     BeginDrawing();
