cd6fbed5e0817ae78aa65a0af8e1dadcade286f1
Cortan122 <Cortan122@gmail.com>
2025-12-12 13:16:02 +0100

    check valid session

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
 #include <raylib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "Editor.h"
 #include "config.h"
 
 int main(int argc, char** argv) {
+  const char* display = getenv("DISPLAY");
+  const char* session = getenv("XDG_SESSION_TYPE");
+  bool is_display_unset = display == NULL || strcmp(display, "") == 0;
+  if (is_display_unset && strcmp(session, "wayland") == 0) {
+    fprintf(stderr, "You don't seem to have the DISPLAY variable set. Did you forget to install xorg-xwayland?\n");
+    return 1;
+  } else if (is_display_unset) {
+    fprintf(stderr, "You don't seem to have the DISPLAY variable set. Are you trying to start me from a tty session?\n");
+    return 1;
+  }
+
   const int screenWidth = DEFAULT_SCREENWIDTH;
   const int screenHeight = DEFAULT_SCREENHEIGHT;
   SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
